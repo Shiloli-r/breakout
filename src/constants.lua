2369d04ff20eae58aab2314d710ca7af836ef7e0
Shiloli-r <ronaldshiloli@gmail.com>
2022-12-16 14:18:10 +0300

    constants

---
+++ src/constants.lua
@@ -0,0 +1,11 @@
+--[[
+    Some global constants for the application
+]]
+
+-- size of our actual window
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+-- size we're trying to emulate with push
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243

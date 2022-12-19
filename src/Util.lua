94afa349441137f8fe0334f297dbccf94bcd9320
Shiloli-r <ronaldshiloli@gmail.com>
2022-12-19 11:48:39 +0300

    Generate Quads Balls

--- src/Util.lua
+++ src/Util.lua
@@ -72,3 +72,33 @@ function GenerateQuadsPaddles(atlas)
 
     return quads
 end
+
+--[[
+    This function is specifically made to piece out the balls from the
+    sprite sheet. For this, we have to piece out the balls a little more
+    manually, since they are in an awkward part of the sheet and small.
+]]
+function GenerateQuadsBalls(atlas)
+    local x = 96
+    local y = 48
+
+    local counter = 1
+    local quads = {}
+
+    for i = 0, 3 do
+        quads[counter] = love.graphics.newQuad(x, y, 8, 8, atlas:getDimensions())
+        x = x + 8
+        counter = counter + 1
+    end
+
+    x = 96
+    y = 56
+
+    for i = 0, 2 do
+        quads[counter] = love.graphics.newQuad(x, y, 8, 8, atlas:getDimensions())
+        x = x + 8
+        counter = counter + 1
+    end
+
+    return quads
+end

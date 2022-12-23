1d7416173a36e95062a8a335114a929373e796d1
Shiloli-r <ronaldshiloli@gmail.com>
2022-12-23 11:28:36 +0300

    Brick particle systems

--- src/Brick.lua
+++ src/Brick.lua
@@ -8,6 +8,40 @@
 
 Brick = Class{}
 
+-- some of the colors in our palette (to be used with particle systems)
+paletteColors = {
+    -- blue
+    [1] = {
+        ['r'] = 99,
+        ['g'] = 155,
+        ['b'] = 255
+    },
+    -- green
+    [2] = {
+        ['r'] = 106,
+        ['g'] = 190,
+        ['b'] = 47
+    },
+    -- red
+    [3] = {
+        ['r'] = 217,
+        ['g'] = 87,
+        ['b'] = 99
+    },
+    -- purple
+    [4] = {
+        ['r'] = 215,
+        ['g'] = 123,
+        ['b'] = 186
+    },
+    -- gold
+    [5] = {
+        ['r'] = 251,
+        ['g'] = 242,
+        ['b'] = 54
+    }
+}
+
 function Brick:init(x, y)
     -- used to coloring and score calculation
     self.tier = 0 
@@ -20,6 +54,23 @@ function Brick:init(x, y)
 
     -- used to determine whether this brick should be rendered
     self.inPlay = true 
+
+    -- particle system belonging to the brick, emitted on hit
+    self.psystem = love.graphics.newParticleSystem(gTextures['particle'], 64)   
+    
+    -- various behavior-determining functions for the particle system
+    -- https://love2d.org/wiki/ParticleSystem
+
+    -- lasts between 0.5-1 seconds seconds
+    self.psystem:setParticleLifetime(0.5, 1)
+
+    -- give it an acceleration of anywhere between X1,Y1 and X2,Y2 (0, 0) and (80, 80) here
+    -- gives generally downward 
+    self.psystem:setLinearAcceleration(-15, 0, 15, 80)
+
+    -- spread of particles; normal looks more natural than uniform, which is clumpy; numbers
+    -- are amount of standard deviation away in X and Y axis
+    self.psystem:setEmissionArea('normal', 10, 10)    
 end
 
 --[[
@@ -27,6 +78,21 @@ end
     changing its color otherwise
 ]]
 function Brick:hit()
+        -- set the particle system to interpolate between two colors; in this case, we give
+    -- it our self.color but with varying alpha; brighter for higher tiers, fading to 0
+    -- over the particle's lifetime (the second color)
+    self.psystem:setColors(
+        paletteColors[self.color].r / 255,
+        paletteColors[self.color].g / 255,
+        paletteColors[self.color].b / 255,
+        55 * (self.tier + 1) / 255,
+        paletteColors[self.color].r / 255,
+        paletteColors[self.color].g / 255,
+        paletteColors[self.color].b / 255,
+        0
+    )
+    self.psystem:emit(64)
+
     -- sound on hit
     gSounds['brick-hit-2']:stop()
     gSounds['brick-hit-2']:play()
@@ -56,6 +122,10 @@ function Brick:hit()
     end
 end 
 
+function Brick:update(dt)
+    self.psystem:update(dt)
+end
+
 function Brick:render()
     if self.inPlay then 
         love.graphics.draw(gTextures['main'],
@@ -66,4 +136,12 @@ function Brick:render()
         self.y
     )
     end
+end
+
+--[[
+    Need a separate render function for our particles so it can be called after all bricks are drawn;
+    otherwise, some bricks would render over other bricks' particle systems.
+]]
+function Brick:renderParticles()
+    love.graphics.draw(self.psystem, self.x + 16, self.y + 8)
 end
--- src/states/PlayState.lua
+++ src/states/PlayState.lua
@@ -142,6 +142,11 @@ function PlayState:update(dt)
         end
     end
 
+    -- for rendering particle systems
+    for k, brick in pairs(self.bricks) do
+        brick:update(dt)
+    end
+
     if love.keyboard.wasPressed('escape') then 
         love.event.quit()
     end
@@ -152,6 +157,11 @@ function PlayState:render()
     for k, brick in pairs(self.bricks) do
         brick:render()
     end
+
+    -- render all particle systems
+    for k, brick in pairs(self.bricks) do
+        brick:renderParticles()
+    end    
 
     self.paddle:render()
     self.ball:render()

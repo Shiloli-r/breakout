6dfb353fcb0d916e9e9a0dfc74a843a4c16487aa
Shiloli-r <ronaldshiloli@gmail.com>
2022-12-23 10:42:04 +0300

    Tier update (change colour on hit)

--- src/Brick.lua
+++ src/Brick.lua
@@ -28,9 +28,32 @@ end
 ]]
 function Brick:hit()
     -- sound on hit
+    gSounds['brick-hit-2']:stop()
     gSounds['brick-hit-2']:play()
 
-    self.inPlay = false
+    -- if we're at a higher tier than the base, we need to go down a tier
+    -- if we're already at the lowest color, else just go down a color
+    if self.tier > 0 then
+        if self.color == 1 then
+            self.tier = self.tier - 1
+            self.color = 5
+        else
+            self.color = self.color - 1
+        end
+    else
+        -- if we're in the first tier and the base color, remove brick from play
+        if self.color == 1 then
+            self.inPlay = false
+        else
+            self.color = self.color - 1
+        end
+    end
+
+    -- play a second layer sound if the brick is destroyed
+    if not self.inPlay then
+        gSounds['brick-hit-1']:stop()
+        gSounds['brick-hit-1']:play()
+    end
 end 
 
 function Brick:render()
